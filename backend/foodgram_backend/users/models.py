d96998865e3c49e8a855c4cf2e368f9a739ae541
Кирилл Терминасов <kterminasov@gmail.com>
2025-05-26 15:38:00 +0300

    Create users app.
    - Create application.
    - Register application.
    - Create custom user model.
    - Register custom user model in admin panel.

---
+++ backend/foodgram_backend/users/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    """Собственная модель пользователя с его фотографией."""
+
+    avatar = models.ImageField(blank=True)
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
